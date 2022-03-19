5cabdacca15e88434b945188d4ff4cccd4bed8ac
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-03-19 08:53:23 +0700

    Add tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React from 'react';
+import './index.css';
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+function App() {
+  return (
+    <>
+      <h1 className="bg-slate-800 text-yellow-200">Welcome to React Vite Micro App!</h1>
+      <p className="test">Hard to get more minimal than this React app.</p>
+    </>
+  );
+}
+
+export default App;
