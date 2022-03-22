cc411c6fd6be518119154794493d534c09b6844a
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-03-22 11:27:15 +0700

    Add Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { FaBook, FaChartBar } from 'react-icons/fa';
-import Button from './components/Button';
-import CardSelection from './components/CardSelection';
-import MenuSelection from './components/MenuSelection';
-import Title from './components/Title';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import './index.css';
 
 function App() {
   return (
-    <>
-      <h1 className="bg-slate-800 text-yellow-200">Welcome to React Vite Micro App!</h1>
-      <p className="test">Hard to get more minimal than this React app.</p>
-      <Title>Hi Ini Judul</Title>
-      <Button>Hello, world</Button>
-      <MenuSelection icon={<FaBook />} text="Input Barang Baru" />
-      <CardSelection icon={<FaChartBar />} text="Keuntungan Bersih" />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  FaBook,
+  FaBox,
+  FaClipboardCheck,
+  FaDollarSign,
+  FaScroll,
+} from 'react-icons/fa';
+import MenuSelection from '../components/MenuSelection';
+import Title from '../components/Title';
+
+function Home() {
+  const selections = [
+    {
+      icon: <FaBook />,
+      text: 'Input Barang Baru',
+    },
+    {
+      icon: <FaBook />,
+      text: 'Input Barang Terjual',
+    },
+    {
+      icon: <FaScroll />,
+      text: 'Input Biaya Operasional',
+    },
+    {
+      icon: <FaBox />,
+      text: 'Laporan Sisa Stok Barang',
+    },
+    {
+      icon: <FaClipboardCheck />,
+      text: 'Laporan Penjualan Barang',
+    },
+    {
+      icon: <FaDollarSign />,
+      text: 'Laporan Keuntungan Penjualan',
+    },
+  ];
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center h-screen">
+      <div className="w-1/4 mb-6">
+        <Title size="4xl">Selamat Datang di Digital Records</Title>
+      </div>
+      {selections.map(({ icon, text }) => (
+        <MenuSelection key={text} icon={icon} text={text} />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
