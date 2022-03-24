337593c74a9cb179026a136fd2f7c50589886182
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-03-24 17:52:43 +0700

    Add path data to navSelections

--- client/src/data/navSelections.jsx
+++ client/src/data/navSelections.jsx
@@ -12,26 +12,32 @@ const selections = [
   {
     icon: <FaBook />,
     text: 'Input Barang Baru',
+    path: '/input-barang-baru',
   },
   {
     icon: <FaBook />,
     text: 'Input Barang Terjual',
+    path: '/input-barang-jual',
   },
   {
     icon: <FaScroll />,
     text: 'Input Biaya Operasional',
+    path: '/input-biaya-operasional',
   },
   {
     icon: <FaBox />,
     text: 'Laporan Sisa Stok Barang',
+    path: '/laporan-sisa-stok',
   },
   {
     icon: <FaClipboardCheck />,
     text: 'Laporan Penjualan Barang',
+    path: '/laporan-penjualan',
   },
   {
     icon: <FaDollarSign />,
     text: 'Laporan Keuntungan Penjualan',
+    path: '/laporan-keuntungan',
   },
 ];
 
