34aab235517de305829d6b7141178484a561ead9
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-05-12 22:59:14 +0700

    Add toastify - for notifications

--- clientv2/src/App.js
+++ clientv2/src/App.js
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import InputBarangBaru from './pages/InputBarangBaru';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/input-barang-baru" element={<InputBarangBaru />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/input-barang-baru" element={<InputBarangBaru />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 }
 
