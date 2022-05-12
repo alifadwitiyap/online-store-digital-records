01a9beb9a39d3111b8a4d004c4bbaaaa83eecb84
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-05-12 23:00:22 +0700

    Add submit logic for InputBarangBaru

--- clientv2/src/pages/InputBarangBaru.jsx
+++ clientv2/src/pages/InputBarangBaru.jsx
@@ -4,6 +4,10 @@ import Sidebar from '../components/Sidebar';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import useAuth from '../utils/useAuth';
+import { convertDateToString } from '../utils/dateConversion';
+import Axios from '../utils/axios';
+import { useSelector } from 'react-redux';
+import { notifyError, notifySuccess } from '../utils/notify';
 
 function InputBarangBaru() {
   const [auth, isAuthenticated] = useAuth();
@@ -15,6 +19,9 @@ function InputBarangBaru() {
     supplier: '',
     jumlah_dibeli: 1,
   });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { token } = useSelector((state) => state.user);
 
   useEffect(() => {
     auth();
@@ -24,14 +31,29 @@ function InputBarangBaru() {
   const onFieldChange = (e) => {
     setFormData((prev) => ({
       ...prev,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     }));
-  }
+  };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(formData);
-  }
+    setIsLoading(true);
+    try {
+      const data = {
+        ...formData,
+        tanggal: convertDateToString(formData.tanggal),
+      };
+      await Axios.post('/barang/inputBarang', data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      notifySuccess('Data berhasil ditambah');
+    } catch (error) {
+      notifyError('Data barang gagal diinput');
+    }
+    setIsLoading(false);
+  };
 
   return (
     <>
@@ -47,40 +69,87 @@ function InputBarangBaru() {
               <DatePicker
                 className="input-field"
                 selected={formData.tanggal}
-                onChange={(tanggal) => setFormData((prev) => ({ ...prev, tanggal }))}
+                onChange={(tanggal) =>
+                  setFormData((prev) => ({ ...prev, tanggal }))
+                }
               />
             </div>
             <div className="mb-4 w-full">
               <label className="block mb-1">
                 Kode Barang <span className="text-red-700">*</span>
               </label>
-              <input type="text" name="id_barang" className="input-field" value={formData.id_barang} placeholder="cth: 086.1021" onChange={onFieldChange} />
+              <input
+                type="text"
+                name="id_barang"
+                className="input-field"
+                value={formData.id_barang}
+                placeholder="cth: 086.1021"
+                onChange={onFieldChange}
+              />
             </div>
             <div className="mb-4 w-full">
               <label className="block mb-1">
                 Nama Barang <span className="text-red-700">*</span>
               </label>
-              <input type="text" name="nama" className="input-field" value={formData.nama} placeholder="cth: Gamis Hitam Maxmara Ceruty" onChange={onFieldChange} />
+              <input
+                type="text"
+                name="nama"
+                className="input-field"
+                value={formData.nama}
+                placeholder="cth: Gamis Hitam Maxmara Ceruty"
+                onChange={onFieldChange}
+              />
             </div>
             <div className="mb-4 w-full">
               <label className="block mb-1">
                 Nama Supplier <span className="text-red-700">*</span>
               </label>
-              <input type="text" name="supplier" className="input-field" value={formData.supplier} placeholder="cth: Grosir gamis syari medan" onChange={onFieldChange} />
+              <input
+                type="text"
+                name="supplier"
+                className="input-field"
+                value={formData.supplier}
+                placeholder="cth: Grosir gamis syari medan"
+                onChange={onFieldChange}
+              />
             </div>
             <div className="mb-4 w-full">
               <label className="block mb-1">
                 Harga Satuan (Rp) <span className="text-red-700">*</span>
               </label>
-              <input type="number" name="harga_beli" className="input-field" value={formData.harga_beli} placeholder="cth: 100.000" onChange={onFieldChange} min="0" />
+              <input
+                type="number"
+                name="harga_beli"
+                className="input-field"
+                value={formData.harga_beli}
+                placeholder="cth: 100.000"
+                onChange={onFieldChange}
+                min="0"
+              />
             </div>
             <div className="mb-4 w-full">
               <label className="block mb-1">
                 Jumlah Barang <span className="text-red-700">*</span>
               </label>
-              <input type="number" name="jumlah_dibeli" className="input-field" value={formData.jumlah_dibeli} placeholder="cth: 2" onChange={onFieldChange} min="1" />
+              <input
+                type="number"
+                name="jumlah_dibeli"
+                className="input-field"
+                value={formData.jumlah_dibeli}
+                placeholder="cth: 2"
+                onChange={onFieldChange}
+                min="1"
+              />
             </div>
-            <button type="submit" className="btn">Tambahkan Barang</button>
+            {isLoading ? (
+              <button type="button" className="btn">
+                loading...
+              </button>
+            ) : (
+              <button type="submit" className="btn">
+                Tambahkan Barang
+              </button>
+            )}
           </form>
         </div>
       </div>
