f1f59ba1b49eb32bfbaf982378ec54b57bc1fb38
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-05-12 22:59:49 +0700

    Add dateConversion util

---
+++ clientv2/src/utils/dateConversion.js
@@ -0,0 +1,6 @@
+
+export const convertDateToString = (date) => {
+  const offset = date.getTimezoneOffset();
+  date = new Date(date.getTime() - (offset*60*1000));
+  return date.toISOString().split('T')[0];
+};
