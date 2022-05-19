4ba8911ca4295910c5107449ee057df95bd1e74a
alifadwitiyap <alifadwitiyap@gmail.com>
2022-05-20 01:20:58 +0700

    add modal to database

--- server/migrations/1648279253123_tabel-akun.js
+++ server/migrations/1648279253123_tabel-akun.js
@@ -1,33 +1,31 @@
 /* eslint-disable camelcase */
 
-
-exports.up = pgm => {
-    pgm.createTable('akun', {
-        id_akun: {
-            type: 'VARCHAR(36)',
-            primaryKey: true
-        },
-        username: {
-            type: 'VARCHAR(30)',
-            notNull: true,
-            unique: true
-        },
-        password: {
-            type: 'TEXT',
-            notNull: true
-        },
-        nama: {
-            type: 'VARCHAR(30)',
-            notNull: true
-        },
-        role: {
-            type: 'VARCHAR(30)',
-            notNull: true
-        },
-
-    })
+exports.up = (pgm) => {
+	pgm.createTable("akun", {
+		id_akun: {
+			type: "VARCHAR(36)",
+			primaryKey: true
+		},
+		username: {
+			type: "VARCHAR(30)",
+			notNull: true,
+			unique: true
+		},
+		password: {
+			type: "TEXT",
+			notNull: true
+		},
+		nama: {
+			type: "VARCHAR(30)",
+			notNull: true
+		},
+		role: {
+			type: "VARCHAR(30)",
+			notNull: true
+		}
+	});
 };
 
-exports.down = pgm => {
-    pgm.dropTable('akun');
+exports.down = (pgm) => {
+	pgm.dropTable("akun");
 };
--- server/migrations/1648279263800_tabel-barang.js
+++ server/migrations/1648279263800_tabel-barang.js
@@ -1,31 +1,33 @@
 /* eslint-disable camelcase */
 
-
-exports.up = pgm => {
-    pgm.createTable('barang', {
-        id_barang: {
-            type: 'VARCHAR(36)',
-            primaryKey: true,
-            unique: true
-        },
-        nama: {
-            type: 'VARCHAR(30)',
-            notNull: true
-        },
-        supplier: {
-            type: 'VARCHAR(30)',
-            notNull: true
-        },
-        jumlah: {
-            type: 'INTEGER',
-            notNull: true
-        },
-        score: {
-            type: 'INTEGER',
-        },
-    })
+exports.up = (pgm) => {
+	pgm.createTable("barang", {
+		id_barang: {
+			type: "VARCHAR(36)",
+			primaryKey: true,
+			unique: true
+		},
+		nama: {
+			type: "VARCHAR(30)",
+			notNull: true
+		},
+		supplier: {
+			type: "VARCHAR(30)",
+			notNull: true
+		},
+		jumlah: {
+			type: "INTEGER",
+			notNull: true
+		},
+		modal: {
+			type: "FLOAT"
+		},
+		score: {
+			type: "FLOAT"
+		}
+	});
 };
 
-exports.down = pgm => {
-    pgm.dropTable('barang');
+exports.down = (pgm) => {
+	pgm.dropTable("barang");
 };
