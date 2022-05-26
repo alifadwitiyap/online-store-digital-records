115c16521e4454ea46ce72edf9dd12f1970c839d
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-05-26 17:53:18 +0700

    Fix sidebar overlapping with backToMenu issue

--- clientv2/src/components/BackToMenu.jsx
+++ clientv2/src/components/BackToMenu.jsx
@@ -7,7 +7,7 @@ function BackToMenu() {
   return (
     <Link
       to="/"
-      className="absolute left-16 bottom-8 font-bold text-purple-500 hover:text-purple-700"
+      className="fixed left-16 bottom-8 font-bold text-purple-500 hover:text-purple-700"
     >
       {'< Menu Utama'}
     </Link>
