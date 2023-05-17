39f04ee1e046b86836661f9cd557cf9f747fcf04
alifadwitiyap <alifadwitiyap@gmail.com>
2023-05-17 12:59:40 +0700

    change api site

--- clientv2/src/utils/axios.js
+++ clientv2/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://inventori-rpl.herokuapp.com',
+  baseURL: 'https://lobster-app-7f2ry.ondigitalocean.app',
 });
 
 export default instance;
